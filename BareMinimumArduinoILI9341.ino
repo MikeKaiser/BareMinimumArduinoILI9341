2fdf11041df940bfb8f8e5eda1700104fb52edee
Michael Salim Kaiser <michael.salim.kaiser@gmail.com>
2021-08-23 13:15:15 +0100

    Clear screen works... Very slow though as we are using digitalWrite().

--- BareMinimumArduinoILI9341.ino
+++ BareMinimumArduinoILI9341.ino
@@ -67,20 +67,54 @@ void Wait()
 {
   //while( !Serial.available() );
   //Serial.read();
-  delay(1);
+  //delayMicroseconds(10);
 }
 
+
+
+
+
+#define ILI9341_SOFTRESET 0x01
+#define ILI9341_SLEEPIN 0x10
+#define ILI9341_SLEEPOUT 0x11
+#define ILI9341_NORMALDISP 0x13
+#define ILI9341_INVERTOFF 0x20
+#define ILI9341_INVERTON 0x21
+#define ILI9341_GAMMASET 0x26
+#define ILI9341_DISPLAYOFF 0x28
+#define ILI9341_DISPLAYON 0x29
+#define ILI9341_COLADDRSET 0x2A
+#define ILI9341_PAGEADDRSET 0x2B
+#define ILI9341_MEMORYWRITE 0x2C
+#define ILI9341_PIXELFORMAT 0x3A
+#define ILI9341_FRAMECONTROL 0xB1
+#define ILI9341_DISPLAYFUNC 0xB6
+#define ILI9341_ENTRYMODE 0xB7
+#define ILI9341_POWERCONTROL1 0xC0
+#define ILI9341_POWERCONTROL2 0xC1
+#define ILI9341_VCOMCONTROL1 0xC5
+#define ILI9341_VCOMCONTROL2 0xC7
+#define ILI9341_MEMCONTROL 0x36
+#define ILI9341_MADCTL 0x36
+
+#define ILI9341_MADCTL_MY 0x80
+#define ILI9341_MADCTL_MX 0x40
+#define ILI9341_MADCTL_MV 0x20
+#define ILI9341_MADCTL_ML 0x10
+#define ILI9341_MADCTL_RGB 0x00
+#define ILI9341_MADCTL_BGR 0x08
+#define ILI9341_MADCTL_MH 0
 
 #define TFT_CS_IDLE     { digitalWrite( LCD_CS, HIGH ); Wait(); }
 #define TFT_CS_ACTIVE   { digitalWrite( LCD_CS, LOW  ); Wait(); }
 
 #define TFT_WR_IDLE		  { digitalWrite( LCD_WR, HIGH ); Wait(); }
 #define TFT_WR_ACTIVE   { digitalWrite( LCD_WR, LOW  ); Wait(); }
-#define TFT_WR_STROBE   { TFT_WR_ACTIVE delay(1); TFT_WR_IDLE }
+#define TFT_WR_STROBE   { TFT_WR_ACTIVE /*delayMicroseconds(1)*/; TFT_WR_IDLE }
 
 #define TFT_RD_IDLE		  { digitalWrite( LCD_RD, HIGH ); Wait(); }
 #define TFT_RD_ACTIVE   { digitalWrite( LCD_RD, LOW  ); Wait(); }
-#define TFT_RD_STROBE   { TFT_RD_ACTIVE delay(1); TFT_RD_IDLE }
+#define TFT_RD_STROBE   { TFT_RD_ACTIVE /*delayMicroseconds(1)*/; TFT_RD_IDLE }
 
 #define TFT_RST_IDLE	  { digitalWrite( LCD_RST, HIGH ); Wait(); }
 #define TFT_RST_ACTIVE  { digitalWrite( LCD_RST, LOW  ); Wait(); }
@@ -88,6 +122,8 @@ void Wait()
 #define TFT_CD_COMMAND	{ digitalWrite( LCD_RS, LOW  ); Wait(); }
 #define TFT_CD_DATA		  { digitalWrite( LCD_RS, HIGH ); Wait(); }
 
+#define TFTWIDTH 240
+#define TFTHEIGHT 320
 
 uint8_t TFT_Read8()
 {
@@ -174,6 +210,36 @@ uint32_t TFT_ReadReg(uint8_t r)
   return (id0<<24)|(id1<<16)|(id2<<8)|id3;
 }
 
+
+
+void TFT_WriteRegister8(uint8_t a, uint32_t d)
+{
+    TFT_CD_COMMAND;
+  delayMicroseconds(10);
+    TFT_Write8(a);
+  delayMicroseconds(10);
+    TFT_CD_DATA;
+  delayMicroseconds(10);
+    TFT_Write8(d);
+}
+
+void TFT_WriteRegister16(uint8_t a, uint32_t d)
+{
+    uint8_t hi, lo;
+    hi = (a) >> 8;
+    lo = (a);
+    TFT_CD_COMMAND;
+  delayMicroseconds(10);
+    TFT_Write8(hi);
+    TFT_Write8(lo);
+    hi = (d) >> 8;
+    lo = (d);
+    TFT_CD_DATA;
+  delayMicroseconds(10);
+    TFT_Write8(hi);
+    TFT_Write8(lo);
+}
+
 void TFT_WriteRegister24(uint8_t r, uint32_t d)
 {
   TFT_CS_ACTIVE;
@@ -205,6 +271,25 @@ void TFT_WriteRegister32(uint8_t r, uint32_t d)
   TFT_Write8(d);
   TFT_CS_IDLE;
 }
+
+
+void TFT_SetAddrWindow( int x1, int y1, int x2, int y2 )
+{
+    uint32_t t;
+    TFT_CS_ACTIVE;
+
+    t = x1;
+    t <<= 16;
+    t |= x2;
+    TFT_WriteRegister32(ILI9341_COLADDRSET, t);
+    t = y1;
+    t <<= 16;
+    t |= y2;
+    TFT_WriteRegister32(ILI9341_PAGEADDRSET, t);
+    TFT_CS_IDLE;
+
+}
+
 
 uint16_t TFT_Init()
 {
@@ -238,6 +323,107 @@ uint16_t TFT_Init()
   return 0;
 }
 
+void TFT_Flood(uint16_t color, uint32_t len)
+{
+  uint16_t blocks;
+  uint8_t i, hi = color >> 8, lo = color;
+
+  TFT_CS_ACTIVE;
+  TFT_CD_COMMAND;
+  TFT_Write8(0x2C);
+
+  // Write first pixel normally, decrement counter by 1
+  TFT_CD_DATA;
+  TFT_Write8(hi);
+  TFT_Write8(lo);
+  len--;
+
+  blocks = (uint16_t)(len / 64); // 64 pixels/block
+  if (hi == lo)
+  {
+    // High and low bytes are identical.  Leave prior data
+    // on the port(s) and just toggle the write strobe.
+    while (blocks--)
+    {
+      i = 16; // 64 pixels/block / 4 pixels/pass
+      do
+      {
+        TFT_WR_STROBE;
+        TFT_WR_STROBE;
+        TFT_WR_STROBE;
+        TFT_WR_STROBE; // 2 bytes/pixel
+        TFT_WR_STROBE;
+        TFT_WR_STROBE;
+        TFT_WR_STROBE;
+        TFT_WR_STROBE; // x 4 pixels
+      } while (--i);
+    }
+    // Fill any remaining pixels (1 to 64)
+    for (i = (uint8_t)len & 63; i--;) {
+      TFT_WR_STROBE;
+      TFT_WR_STROBE;
+    }
+  } else {
+    while (blocks--)
+    {
+      i = 16; // 64 pixels/block / 4 pixels/pass
+      do
+      {
+        TFT_Write8(hi);
+        TFT_Write8(lo);
+        TFT_Write8(hi);
+        TFT_Write8(lo);
+        TFT_Write8(hi);
+        TFT_Write8(lo);
+        TFT_Write8(hi);
+        TFT_Write8(lo);
+      } while (--i);
+    }
+    for (i = (uint8_t)len & 63; i--;)
+    {
+      TFT_Write8(hi);
+      TFT_Write8(lo);
+    }
+  }
+  TFT_CS_IDLE;
+}
+
+
+void TFT_FillScreen(uint16_t color)
+{
+  // For these, there is no settable address pointer, instead the
+  // address window must be set for each drawing operation.  However,
+  // this display takes rotation into account for the parameters, no
+  // need to do extra rotation math here.
+  TFT_SetAddrWindow(0, 0, TFTWIDTH - 1, TFTHEIGHT - 1);
+  TFT_Flood(color, (long)TFTWIDTH * (long)TFTHEIGHT);
+}
+
+void TFT_Begin()
+{
+    TFT_CS_ACTIVE;
+    TFT_WriteRegister8(ILI9341_SOFTRESET, 0);
+    delay(50);
+    TFT_WriteRegister8(ILI9341_DISPLAYOFF, 0);
+
+    TFT_WriteRegister8(ILI9341_POWERCONTROL1, 0x23);
+    TFT_WriteRegister8(ILI9341_POWERCONTROL2, 0x10);
+    TFT_WriteRegister16(ILI9341_VCOMCONTROL1, 0x2B2B);
+    TFT_WriteRegister8(ILI9341_VCOMCONTROL2, 0xC0);
+    TFT_WriteRegister8(ILI9341_MEMCONTROL, ILI9341_MADCTL_MY | ILI9341_MADCTL_BGR);
+    TFT_WriteRegister8(ILI9341_PIXELFORMAT, 0x55);
+    TFT_WriteRegister16(ILI9341_FRAMECONTROL, 0x001B);
+
+    TFT_WriteRegister8(ILI9341_ENTRYMODE, 0x07);
+    /* TFT_WriteRegister32(ILI9341_DISPLAYFUNC, 0x0A822700);*/
+
+    TFT_WriteRegister8(ILI9341_SLEEPOUT, 0);
+    delay(150);
+    TFT_WriteRegister8(ILI9341_DISPLAYON, 0);
+    delay(500);
+    TFT_SetAddrWindow(0, 0, TFTWIDTH - 1, TFTHEIGHT - 1);
+}
+
 
 
 //////////////////////////////////////////////////////////////
@@ -264,7 +450,9 @@ void setup()
   uint16_t id = TFT_Init();
   Serial.print("ID = ");
   Serial.println(id,16);
-
+  TFT_Begin();
+  TFT_FillScreen( 0x0FF0 );
+  
   //WaitForSDCardInsert();
   //ListSDCard();
  
